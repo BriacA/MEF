83e4f8b3bf918f730d8e7cb49cc972104e780e20
Auger Briac <Briac@MacBook-Pro-de-Auger.local>
2016-05-23 12:06:58 +0200

    Ajout Segment.cpp

--- src/Segment.cpp
+++ src/Segment.cpp
@@ -1,11 +1,42 @@
 #include "Segment.h"
 
-Segment::Segment()
+// File modification date : 17/05/2016
+
+double Segment::getLongueur()
 {
-    //ctor
+    double L = p1.distanceA(p2) ;
+    return L ;
 }
 
-Segment::~Segment()
+int Segment::diviserTaille(double taille)
 {
-    //dtor
+// Method to divide the segment :
+//
+//
+
+    double L = p1.distance1(p2) ;
+
+    double N = L/taille ;
+    int entier_N = floor(N) + 1 ;
+    double decimale_N = N - entier_N + 1 ;
+
+    double new_taille = taille - decimale_N/entier_N ;
+}
+
+void Segment::diviserEnNParties(int N)
+{
+// Create N new segments from the initial one :
+//
+// For each loop : First, find the new length of each new segment. Then,
+// call diviserTaille for finding how to divide the initial segment.
+// Finally, create the new segment.
+
+    for(int i=0 ; i<N ; i++)
+    {
+        double taille = p1.distanceA(p2)/N ;
+
+        Segment diviserTaille(taille) ;
+
+        Courbe::courbe(p1, p2, NULL, 0) ;
+    }
 }
