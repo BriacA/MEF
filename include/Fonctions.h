9af0b9f78c25991e9fb8e9b75c917e9c45472b19
Clement <Clement>
2016-05-23 14:00:56 +0200

    Commit Fonctions.h

--- include/Fonctions.h
+++ include/Fonctions.h
@@ -4,11 +4,18 @@
 
 class Fonctions
 {
-    public:
-        Fonctions();
-        virtual ~Fonctions();
-    protected:
-    private:
+public:
+    Fonctions();
+    virtual ~Fonctions();
+
+    double calculAngle(Point p1, Point p2, Point p3, int sens);
+    double getLongueur(Point p1, Point p2);
+    double getLongueur(double x1, double y1, double x2, double y2);
+
+protected:
+private:
+
+    double calculAngle(double x, double y);
 };
 
 #endif // FONCTIONS_H
