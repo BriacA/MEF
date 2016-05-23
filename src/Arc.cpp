9fe2a840890bccb60f2e15281a42248560b02e09
Auger Briac <Briac@MacBook-Pro-de-Auger.local>
2016-05-23 11:45:23 +0200

    Ajout fichier Arc

--- src/Arc.cpp
+++ src/Arc.cpp
@@ -1,11 +1,28 @@
 #include "Arc.h"
 
-Arc::Arc()
+// File modification date : 17/05/2016
+
+void Arc::arc(Point centre, double angle)
 {
-    //ctor
+    _centre = centre ;
+    _angle = angle ;
 }
 
-Arc::~Arc()
+Point getCentre()
 {
-    //dtor
+    return this->centre ;
 }
+
+double getAngle()
+{
+    return this->angle ;
+}
+
+void Arc::diviserEnNParties(int N)
+{
+    for(int i=0 ; i<N ; i++)
+    {
+
+    }
+}
+
