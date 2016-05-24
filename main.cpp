41b85f1e63e1e6980040c879baec84645212e5ab
Clément <Clment@192.168.200.163>
2016-05-24 14:11:51 +0200

    essai

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+#include <cmath>
+
+
+#include "Point.cpp"
+#include "Contour.cpp"
+#include "Courbe.h"
+#include "Arc.h"
+#include "Segment.h"
+
+#include "Fonctions.h"
+#include "Mailleur.h"
+
+
+#define PI 3.14159265
+
+
 using namespace std;
 
 int main()
